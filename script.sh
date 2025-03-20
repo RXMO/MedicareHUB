6625cd00f96621b2395b4619edb7698e2d05f6e0
RXMO <omxrmxnsouri@gmail.com>
2025-03-20 22:34:40 +0100

    Changement du terminal

--- script.sh
+++ script.sh
@@ -63,6 +63,6 @@ sleep 5
 
 # Lancer le front-end
 echo "--------------------Lancement du Front-end--------------------"
-xterm -e "cd xmart-frontend/target && java -jar xmart-frontend-1.0-SNAPSHOT-jar-with-dependencies.jar"
+gnome-terminal -- bash -c "cd xmart-frontend/target && java -jar xmart-frontend-1.0-SNAPSHOT-jar-with-dependencies.jar; exec bash"
 
 echo "✅ Front-End démarré."
